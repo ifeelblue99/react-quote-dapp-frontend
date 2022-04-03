b2c884db42a7604c091478f1065434cca7a31bc4
ifeelblue99 <deadjoy01@gmail.com>
2022-04-03 17:11:48 +0300

    3 files modified

--- src/contract/abi-address.js
+++ src/contract/abi-address.js
@@ -165,4 +165,4 @@ export const quoteAbi = [
     "type": "function"
   }
 ],
-export const quoteContractAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+export const quoteContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
